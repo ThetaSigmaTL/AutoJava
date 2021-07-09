d678cc39ec28a85c2b11ca1dbcff20d13711e5de
Raja3310 <66636788+Raja3310@users.noreply.github.com>
2021-07-09 23:00:38 +0300

    123

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,9 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	System.out.print("11111");
+	    System.out.println("11111");
+        System.out.print("23432");
+
     }
+
 }
