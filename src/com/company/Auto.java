adc138a1d41e73c38cfa66961b2a4611055ca3da
Raja3310 <66636788+Raja3310@users.noreply.github.com>
2021-07-09 23:14:29 +0300

    Add class auto

---
+++ src/com/company/Auto.java
@@ -0,0 +1,5 @@
+package com.company;
+
+public class Auto {
+    public int count = 5;
+}
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,13 +1,13 @@
 package com.company;
 
 public class Main {
-
+    static Auto myAuto = new Auto();
     public static void main(String[] args) {
 	    System.out.println("11111");
         System.out.print("23432");
         System.out.print(",,,,,,,,,");
-        System.out.print("ahaa");
-
+        System.out.println("ahaa");
+        System.out.print(myAuto.count);
     }
 
 }
